efda099d741a9926121afc1b49b5e3a6a42013d0
Bich Le <leb@platform9.net>
2018-06-13 18:21:14 -0700

    INF-761: make springboard handle unexpected child exit

--- cmd/springboard/springboard.go
+++ cmd/springboard/springboard.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"time"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -18,10 +19,6 @@ func main() {
 	}
 	listenAddr := fmt.Sprintf("127.0.0.1:%s", portStr)
 	log.Printf("listening on %s", listenAddr)
-	ln, err := net.Listen("tcp", listenAddr)
-	if err != nil {
-		log.Fatalf("failed to listen: %s", err);
-	}
 
 	delayStr := os.Getenv("SPRINGBOARD_DELAY_SECONDS")
 	if delayStr != "" {
@@ -39,23 +36,57 @@ func main() {
 	cmd := exec.Command(prog, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err = cmd.Start()
+	err := cmd.Start()
 	if err != nil {
 		log.Fatalf("failed to start process: %s", err);
 	}
 	log.Printf("child process pid is %d", cmd.Process.Pid)
-	conn, err := ln.Accept()
-	if err != nil {
-		log.Fatalf("failed to accept connection: %s", err);
+	cancel := make(chan bool)
+	go func () {
+		err = cmd.Wait()
+		if err != nil {
+			log.Warnf("failed to wait for child process: %s", err);
+		}
+		log.Printf("child success indicator: %t",
+			cmd.ProcessState.Success())
+		cancel <- true
+	} ()
+	keepLooping := true
+	childExited := false
+	for keepLooping {
+		keepLooping = func () bool {
+			ln, err := net.Listen("tcp", listenAddr)
+			if err != nil {
+				log.Fatalf("failed to listen: %s", err);
+			}
+			defer ln.Close()
+			tcpListener := ln.(*net.TCPListener)
+			tcpListener.SetDeadline(time.Now().Add(2 * time.Second))
+			conn, err := ln.Accept()
+			if err != nil {
+				if strings.Contains(err.Error(), "i/o timeout") {
+					// retry
+					return true
+				}
+				log.Fatalf("failed to accept connection: %s", err);
+				// not reached
+			} else {
+				conn.Close()
+			}
+			return false
+		} ()
+		select {
+		case _ = <-cancel:
+			log.Infof("child process exited on its own")
+			keepLooping = false
+			childExited = true
+		default:
+		}
 	}
-	conn.Close()
-	log.Printf("accepted connection. Terminating child process...")
-	cmd.Process.Signal(syscall.SIGTERM)
-	err = cmd.Wait()
-	if err != nil {
-		log.Warnf("failed to wait for child process: %s", err);
+	if !childExited {
+		log.Printf("accepted connection. Terminating child process...")
+		cmd.Process.Signal(syscall.SIGTERM)
 	}
-	log.Printf("child success indicator: %t", cmd.ProcessState.Success())
 }
 
 
