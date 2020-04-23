e42b875036b06c709f68b4a96c063d37eb15bd42
erwinvaneyk <erwinvaneyk@gmail.com>
2020-04-23 23:53:35 +0200

    Fix registering of the scheme in the App client

--- pkg/client/cust_resource.go
+++ pkg/client/cust_resource.go
@@ -5,10 +5,26 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 
 	deccov1 "github.com/platform9/decco/api/v1beta2"
 )
+
+var (
+	scheme = runtime.NewScheme()
+)
+
+func init() {
+	// Add Decco types to the scheme
+	err := deccov1.AddToScheme(scheme)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add the default types to this scheme.
+	clientgoscheme.AddToScheme(scheme)
+}
 
 // TODO: make this private so that we don't expose RESTClient once operator code uses this client instead of REST calls
 func New(cfg *rest.Config) (*rest.RESTClient, error) {
@@ -16,7 +32,7 @@ func New(cfg *rest.Config) (*rest.RESTClient, error) {
 	config.GroupVersion = &deccov1.GroupVersion
 	config.APIPath = "/apis"
 	config.ContentType = runtime.ContentTypeJSON
-	config.NegotiatedSerializer = serializer.NewCodecFactory(runtime.NewScheme())
+	config.NegotiatedSerializer = serializer.NewCodecFactory(scheme)
 
 	client, err := rest.RESTClientFor(&config)
 	if err != nil {
@@ -30,7 +46,7 @@ func NewAppClient(cfg *rest.Config) (*rest.RESTClient, error) {
 	config := *cfg
 	config.GroupVersion = &deccov1.GroupVersion
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.NewCodecFactory(runtime.NewScheme())
+	config.NegotiatedSerializer = serializer.NewCodecFactory(scheme)
 	restCli, err := rest.RESTClientFor(&config)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create app rest client: %s", err)
